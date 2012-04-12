d723383af01543efeb14a21cc7148ab77a057bea
Lucas Virgili <luvirgili@gmail.com>
2012-04-12 14:27:00 -0300

    64

---
+++ Lvl3/euler64.cpp
@@ -0,0 +1,35 @@
+// Used the method here:
+// http://en.wikipedia.org/wiki/Methods_of_computing_square_roots#Continued_fraction_expansion
+// to calculate the expansion and found here:
+// http://www.math.princeton.edu/mathlab/jr02fall/Periodicity/mariusjp.pdf
+// that the limit for the expansion is 2*(a_0).
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+     unsigned sum = 0;
+     unsigned n = 10000;
+     for (unsigned i = 2; i <= n; ++i) {
+          if (floor(sqrt(i)) != sqrt(i)) {
+               double n = sqrt(i);
+               unsigned a = floor(n);
+               int m = 0;
+               int d = 1;
+               unsigned limit = 2 * a;
+               unsigned c = 0;
+               while (a != limit) {
+                    ++c;
+                    m = d * a - m;
+                    d = (i - m*m) / d;
+                    a = floor((sqrt(i)+m)/d);
+               }
+               if (c % 2 != 0) {
+                    ++sum;
+               }
+          }
+     }
+     cout << sum << endl;
+     return 0;
+}
