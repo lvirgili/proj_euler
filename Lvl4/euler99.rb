d89ca03a8886b97b7dd6ba6b10df753a5109067c
Lucas Virgili <luvirgili@gmail.com>
2012-04-28 00:37:37 -0300

    99

--- Lvl4/euler99.rb
+++ Lvl4/euler99.rb
@@ -1,17 +1,18 @@
 def euler
+  max = 0
+  num = 0
   f = File.new("/tmp/base_exp.txt")
-  numbers = Array.new()
   cont = 1
   while line = f.gets
     line.chomp
     x,y = line.split(',')
     x = x.to_i
     y = y.to_i
-    numbers << [cont, y * Math.log10(x)]
+    z = y * Math.log10(x)
+    if z > max then max, num = z, cont end
     cont += 1
   end
-  numbers.sort! {|x,y| x[1] <=> y[1]}
-  p numbers.last
+  p num
   f.close
 end
 
